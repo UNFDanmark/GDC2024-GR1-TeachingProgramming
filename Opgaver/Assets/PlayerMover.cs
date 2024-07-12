6249c5d92179ba0310e0c7b38eb53fd5a1291724
Deltager <teknisk+GDC2024-GR1@game.unf.dk>
2024-07-12 14:53:33 +0200

    Opgave 1 + Opgave 2 i teaching

---
+++ Opgaver/Assets/PlayerMover.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public int Speed = 10;
+    public double GunCooldown = 0.5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hej jeg er player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Hej jeg er player");
+        
+        
+    }
+}
+
